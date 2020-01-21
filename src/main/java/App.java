8b730ceff76ad5e7c209d98b63197159de51f71e
Mburiah <bykieswagix5@gmail.com>
2020-01-21 12:02:12 +0300

    .

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,20 @@
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oEmployeeDao;
+import dao.Sql2oNewsDao;
+import org.sql2o.Sql2o;
+
+import java.sql.Connection;
+
 public class App {
+    public static void main(String[] args){
+        Sql2oDepartmentDao departmentDao;
+        Sql2oEmployeeDao employeeDao;
+        Sql2oNewsDao newsDao;
+        Connection conn;
+//        Gson gson = new Gson();
+
+        String postgres = "jdbc:postgresql://localhost:5432/org_api_test";
+        Sql2o sql2o = new Sql2o(postgres, "postgres", "password");
+
+    }
 }
